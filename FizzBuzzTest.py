294600ad911893495ea533c6abf25f003ce22d50
edward19 <edward19@abine.org>
2019-04-08 09:06:56 -0400

    Initial Commit

---
+++ FizzBuzzTest.py
@@ -0,0 +1,24 @@
+import pytest
+
+# Function states "Fizz" for numbers that are multiples of 3, "Buzz" for multiples of 5 and 
+# "FizzBuzz" for multioples of 3 and 5.
+def fizzBuzz( value ):
+  return
+
+
+# Can I call FizzBuzz
+def test_canCallFizzBuzz():
+  fizzBuzz(1)
+  
+
+
+
+
+
+# Get "1" when I pass in 1
+# Get "2" when I pass in 2
+# Get "Fizz" when I pass in 3
+# Get "Buzz" when i pass in 5
+# Get "Fizz" when I pass in 6 (a multiple of 3)
+# Get "Buzz" when I pass in 10 (a multiple of 5)
+# Get Fizz Buzz when I pass in 15 (a multiple of 3 and 5)
---
+++ README.md
@@ -0,0 +1,21 @@
+# TDD and Python FizzBuzz Kata
+
+>Practicing TDD with Python
+
+## Dependencies
+
+- Python 3.7.0,  Check version, `python -V`
+- PyTest, 
+  - Install pytest, `pip install pytest`
+  - Check pytest version, `pytest --version`
+
+
+
+
+
+
+## Sources
+
+- [Udemy: Unit Testing and TDD in Python](https://www.udemy.com/unit-testing-and-tdd-in-python/)
+- [pytest.org](https://docs.pytest.org)
+- [pytest Docs: Install and Getting Started](https://docs.pytest.org/en/3.0.0/getting-started.html)
