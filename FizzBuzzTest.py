4a3dc1294e0ceea7a6ae31fa1194647c9caa5f10
edward19 <edward19@abine.org>
2019-04-08 10:40:50 -0400

    Added Fizz and test

--- FizzBuzzTest.py
+++ FizzBuzzTest.py
@@ -3,6 +3,8 @@ import pytest
 # Function states "Fizz" for numbers that are multiples of 3, "Buzz" for multiples of 5 and 
 # "FizzBuzz" for multioples of 3 and 5.
 def fizzBuzz( value ):
+  if value == 3:
+    return "Fizz"
   return str(value)
 
 
@@ -10,21 +12,25 @@ def fizzBuzz( value ):
 ######## TESTS ########
 
 
-# Can I call FizzBuzz
-def test_canCallFizzBuzz():
-  fizzBuzz(1) 
-  
+# Create a utility function to be used by the testsFunction
+# Function checks fizzBuzz value and expected return value
+def checkFizzBuzz( value, expectedReturnValue ):
+  returnValue = fizzBuzz(value)
+  assert returnValue == expectedReturnValue
+
+
 # Get "1" when I pass in 1
 def test_returns1With1PassedIn():
-  returnValue = fizzBuzz(1)
-  assert returnValue == "1"
+  checkFizzBuzz (1, "1")
   
 # Get "2" when I pass in 2
 def test_returns2With2PassedIn():
-  returnValue = fizzBuzz(2)
-  assert returnValue == "2"
-
+  checkFizzBuzz (2, "2")
+  
 # Get "Fizz" when I pass in 3
+def test_returnsFizzWith3PassedIn():
+  checkFizzBuzz(3,"Fizz")
+
 # Get "Buzz" when i pass in 5
 # Get "Fizz" when I pass in 6 (a multiple of 3)
 # Get "Buzz" when I pass in 10 (a multiple of 5)
